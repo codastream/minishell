64755a2176ba91f54c09396ad516f8c3936b60c0
Johan Massavie <jmassavi@3D1.42angouleme.fr>
2025-01-19 16:55:34 +0100

    19/01/2025

--- srcs/main.c
+++ srcs/main.c
@@ -1,22 +1,36 @@
 #include "../includes/shell.h"
 
+void handle_sigint(int sig)
+{
+	if (sig == 2)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
+
 int	main(int ac, char **av, char **env)
 {
+	struct sigaction	sa;
+	char				*line;
+
 	(void) av;
 	(void) env;
 	if (ac == 1)
 	{
-		char	*prompt;
-		char	*line;
-
-		prompt = NULL;
+		sa.sa_handler = handle_sigint;
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = 0;
+		sigaction(SIGINT, &sa, NULL);
 		while (true)
 		{
-			prompt = update_prompt();
-			line = readline((const char*)prompt);
-			printf("%s\n", line);
-			free(prompt);
-			free(line);
+			line = readline("\001\033[36;1m\002C_Shell > \001\033[0m\002");
+			if (!ft_strncmp(line, "exit", 3) || !line)
+				break;
+			ft_printf("%s\n", line);
+			free (line);
 		}
 	}
 	else
