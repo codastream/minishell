c263fc6881d241e0037b155b88df85b062a16dd8
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-02-11 18:17:47 +0100

    [wip] heredoc

--- srcs/execution/heredoc.c
+++ srcs/execution/heredoc.c
@@ -1,10 +1,25 @@
 #include "shell.h"
 
+void	process_input(t_data *data, t_command *command, int fds[2])
+{
+	char	*input;
+
+	close(fds[0]);
+	input = get_next_line(data->exec->original_in);
+	while (input && ft_strcmp(input, command->heredoc))
+	{
+		ft_print_str_fd(fds[1], input);
+		free(input);
+		input = get_next_line(data->exec->original_in);
+	}
+	free(input);
+	close(fds[1]);
+}
+
 void	init_heredoc(t_data *data, t_token *token)
 {
 	int		fds[2];
 	int		child_pid;
-	char	*input;
 
 	if (!token->command->heredoc)
 		return ;
@@ -12,7 +27,7 @@ void	init_heredoc(t_data *data, t_token *token)
 	child_pid = safe_fork(data);
 	if (child_pid == 0)
 	{
-		input = get_next_line()
+		process_input(data, token->command, fds);
 	}
 	else
 	{
