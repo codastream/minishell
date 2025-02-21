85b66934f2d9b952ba21c8dcd4bcae6856ed0c28
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-02-21 18:01:46 +0100

    [fix] update error message syntax error near ...

--- includes/errors.h
+++ includes/errors.h
@@ -6,6 +6,6 @@
 # define EXIT_NOT_FOUND_COMMAND 127
 
 # define MSG_MEMORY_ERROR "memory error"
-# define MSG_SYNTAX_QUOTE_ERROR "unexpected EOF while looking for matching `\""
+# define MSG_SYNTAX_QUOTE_ERROR "unexpected EOF while looking for matching `\"'"
 
 #endif
