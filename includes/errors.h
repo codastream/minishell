0e427937c139e7676f9cb421d9deea01bf53a90b
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-02-21 17:27:24 +0100

    [fix] return value for syntax error

--- includes/errors.h
+++ includes/errors.h
@@ -2,10 +2,10 @@
 # define ERRORS_H
 
 # define EXIT_IGNORE 0
-# define EXIT_SYNTAX_ERROR 1
+# define EXIT_SYNTAX_ERROR 2
 # define EXIT_NOT_FOUND_COMMAND 127
 
 # define MSG_MEMORY_ERROR "memory error"
-# define MSG_SYNTAX_QUOTE_ERROR "invalid syntax - quotes are not properly closed"
+# define MSG_SYNTAX_QUOTE_ERROR "unexpected EOF while looking for matching `\""
 
 #endif
