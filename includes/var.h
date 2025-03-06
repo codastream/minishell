299cb2d930e9c4839e2436ae5ab12e93cba51d83
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-03-06 16:25:54 +0100

    [fix] last return code

--- includes/var.h
+++ includes/var.h
@@ -1,7 +1,7 @@
 #ifndef VAR_H
 # define VAR_H
 
-# define LAST_RETURN_CODE "LRC"
+# define LAST_RETURN_CODE "?"
 
 void	init_vars(t_data *data, char **env);
 char	**hashtab_to_tab(t_data *data, t_hash *hash);
