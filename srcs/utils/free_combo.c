4c392389844edacd263439fb6619ce1a29747d70
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-03-06 15:33:38 +0100

    [fix] set pointers to null in free combo

--- srcs/utils/free_combo.c
+++ srcs/utils/free_combo.c
@@ -4,8 +4,10 @@ void	free_before_parsing(t_data *data)
 {
 	if (data->line)
 		reset(data->line);
+	data->line = NULL;
 	if (data->prompt)
 		reset(data->prompt);
+	data->prompt = NULL;
 }
 
 void	free_after_parsing(t_data *data)
@@ -22,12 +24,16 @@ void	free_after_exec(t_data *data)
 		pop_all_fd(&data->fds);
 	if (data->tokens)
 		free(data->tokens);
+	data->tokens = NULL;
 	if (data->tree)
 		free_tree(data->tree);
+	data->tree = NULL;
 	if (data->exec)
 		free_exec(data->exec);
+	data->exec = NULL;
 	if (data->varstab)
 		ft_free_2d_char_null_ended(data->varstab);
+	data->varstab = NULL;
 }
 
 void	free_vars_and_data(t_data *data) // can be replaced with free_all_data
