0592f478ebe2d8b68db328deb2df0cbfe51cede3
Johan Massavie <jmassavi@2G3.42angouleme.fr>
2025-03-11 20:28:07 +0100

    fix exit and quote

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -5,8 +5,8 @@ int	handle_return_code(t_data *data, t_token *token)
 	int		return_code;
 	char	*msg;
 
-	if (is_atoi_str(token->command->command_args[1]))
-		return_code = ft_atoi(token->command->command_args[1]) % 256;
+	if (is_atoll_str(token->command->command_args[1]))
+		return_code = ft_atoll(token->command->command_args[1]) % 256;
 	else
 	{
 		msg = build_wrongvar_msg(data, token->command->command_name, \
@@ -25,11 +25,11 @@ void	ft_exit(t_data *data, t_token *token)
 
 	arg_count = ft_count_2dchar_null_ended(token->command->command_args);
 	return_code = EXIT_SUCCESS;
-	if (arg_count > 2)
-		handle_builtin_error(data, token->command, \
-		MSG_TOO_MANY_ARGUMENTS, EXIT_FAILURE);
 	if (token->command->command_args[1])
 		return_code = handle_return_code(data, token);
+	if (arg_count > 2 && return_code != 2)
+		handle_builtin_error(data, token->command, \
+		MSG_TOO_MANY_ARGUMENTS, EXIT_FAILURE);
 	free_all_data(data);
 	exit(return_code);
 }
