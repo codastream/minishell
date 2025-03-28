0a98832f5833e30eb78728520aec2b5fd2204a08
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-03-28 14:29:43 +0100

    [fix] empty expand in args

--- srcs/parsing/lexing/args.c
+++ srcs/parsing/lexing/args.c
@@ -6,7 +6,7 @@
 /*   By: fpetit <fpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 17:59:21 by fpetit            #+#    #+#             */
-/*   Updated: 2025/03/27 19:02:24 by fpetit           ###   ########.fr       */
+/*   Updated: 2025/03/28 14:15:47 by fpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,25 +37,25 @@ static int	count_valid_args(t_token *token)
 	return (argc);
 }
 
-static void	update_args(t_data *data, t_token *token, int argc)
+static void	update_args(t_token *token, char **new_args)
 {
-	char	**new_args;
 	char	*arg;
 	bool	has_command;
 	int		i;
 	int		e;
 
-	new_args = ft_calloc(argc + 1, sizeof(char *));
-	check_alloc(data, new_args);
 	i = 0;
 	e = 0;
 	has_command = false;
 	while (token->command->command_args[i])
 	{
 		arg = token->command->command_args[i];
-		if (!has_command)
+		if (!has_command && arg)
+		{
 			has_command = true;
-		if (arg)
+			new_args[e++] = arg;
+		}
+		else if (has_command && arg)
 			new_args[e++] = arg;
 		else
 			free(arg);
@@ -68,6 +68,7 @@ static void	update_args(t_data *data, t_token *token, int argc)
 int	remove_empty_args(t_data *data, t_token **tokens, t_token *token)
 {
 	int		argc;
+	char	**new_args;
 
 	(void) tokens;
 	if (token->type != T_COMMAND || !token->command->command_args)
@@ -75,7 +76,9 @@ int	remove_empty_args(t_data *data, t_token **tokens, t_token *token)
 	argc = count_valid_args(token);
 	if (argc == 0)
 		return (EXIT_IGNORE);
-	update_args(data, token, argc);
+	new_args = ft_calloc(argc + 1, sizeof(char *));
+	check_alloc(data, new_args);
+	update_args(token, new_args);
 	if (ft_strcmp(token->command->command_args[0], \
 		token->command->command_name))
 	{
