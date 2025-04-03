807aa6ac4d7ea9a11bb6c5868654c1fe12e46c7f
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-04-03 20:34:34 +0200

    [fix[ echo ''''$USER''''

--- srcs/utils/quote_utils.c
+++ srcs/utils/quote_utils.c
@@ -6,7 +6,7 @@
 /*   By: fpetit <fpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 19:11:04 by fpetit            #+#    #+#             */
-/*   Updated: 2025/03/27 19:11:05 by fpetit           ###   ########.fr       */
+/*   Updated: 2025/04/03 20:33:47 by fpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,14 @@ void	skip_quote(char	*string, int *i, char quote)
 
 void	skip_single_quote(char *string, int *i)
 {
-	(*i)++;
-	while (string[*i] && string[*i] != '\'')
+	while (string[*i] == '\'')
+	{
 		(*i)++;
-	if (string[*i] == '\'')
-		(*i)++;
+		while (string[*i] && string[*i] != '\'')
+			(*i)++;
+		if (string[*i] == '\'')
+			(*i)++;
+	}
 }
 
 bool	is_quote(char c)
